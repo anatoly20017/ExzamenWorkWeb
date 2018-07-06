819f25225dbf34afa7a066e11bf5521eef22e88b
Pivovar Alexandr <pivovaralexandrlex@gmail.com>
2018-07-06 09:49:21 +0300

    version 1.0

---
+++ WebApplication/WebApplication/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using HelloAngularApp.Models;
+
+namespace HelloAngularApp.Controllers
+{
+    [Route("api/products")]
+    public class ProductController : Controller
+    {
+        ApplicationContext db;
+        public ProductController(ApplicationContext context)
+        {
+            db = context;
+            if (!db.Products.Any())
+            {
+                db.Products.Add(new Product { Autor = "Марина", Name = "Яблочный пирог", Discription = "" });
+                db.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return db.Products.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            Product product = db.Products.FirstOrDefault(x => x.Id == id);
+            return product;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return Ok(product);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Update(product);
+                db.SaveChanges();
+                return Ok(product);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Product product = db.Products.FirstOrDefault(x => x.Id == id);
+            if (product != null)
+            {
+                db.Products.Remove(product);
+                db.SaveChanges();
+            }
+            return Ok(product);
+        }
+    }
+}
